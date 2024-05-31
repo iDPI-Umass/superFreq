ebc4c1eebd11aca15c4b9dcf687b79baf488ef66
sug48 <mpsugarman@gmail.com>
2024-05-31 19:30:39 -0400

    localstorage clear

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,10 @@
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ locals: { session } }) => {
+  if (!session ) {
+    localStorage.clear()
+  }
+
   return {
     session,
   }
