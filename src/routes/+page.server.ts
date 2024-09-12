d58b2f3159126d6b1994076134e77a5c330ab98f
sug48 <mpsugarman@gmail.com>
2024-09-12 13:30:09 -0400

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,13 @@
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
+export const load: PageServerLoad = async ({ url, parent, locals: { safeGetSession } }) => {
   const session = await safeGetSession()
-
   // if the user is already logged in, redirect to feed
   if (session.session) {
-    throw redirect(303, '/feed')
+    const { profile } = await parent()
+    const username = profile.username as string
+    throw redirect(303, `/user/${username}`)
   }
   else {
     throw redirect(303, '/welcome')
