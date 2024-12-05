64af641bf292515e5b4ab7065762dbaa2b91c3e1
sug48 <mpsugarman@gmail.com>
2024-12-05 15:15:52 -0500

    Update NowPlayingPost.svelte

--- src/lib/components/Posts/NowPlayingPost.svelte
+++ src/lib/components/Posts/NowPlayingPost.svelte
@@ -63,8 +63,6 @@
         'artist_name': post.avatar_artist_name,
         'release_group_name': post.avatar_release_group_name
     }
-
-    console.log(avatarItem)
 </script>
 
 <svelte:options runes={true} />
