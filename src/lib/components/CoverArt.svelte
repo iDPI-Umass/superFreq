320e6c9c5209ba4d76736d76c6e20df29f0f5bcb
sug48 <mpsugarman@gmail.com>
2025-01-14 18:36:13 -0500

    Update CoverArt.svelte

--- src/lib/components/CoverArt.svelte
+++ src/lib/components/CoverArt.svelte
@@ -6,6 +6,7 @@ Currently configured to server Last.fm images on the client side by default on a
 
 
 <script lang="ts">
+    import { onMount } from "svelte"
     import { checkFetchedCoverArt, getLastFmCoverArt } from "$lib/resources/musicbrainz"
     import wave from "$lib/assets/images/logo/freq-wave.svg"
 
@@ -37,13 +38,24 @@ Currently configured to server Last.fm images on the client side by default on a
     const coverArtArchiveImgUrl = $derived(item ? coverArtItem['img_url'] : null)
     const lastFmImgUrl = $derived(item ? (item['last_fm_img_url'] ?? item['last_fm_avatar_img_url']) : null)
 
+    let coverArt = $state(wave)
+    let text = $state('loading')
+
+    onMount(async () => {
+        const getCoverArt = await getLastFmCoverArt(coverArtItem)
+        coverArt = getCoverArt ? getCoverArt : wave
+        text = getCoverArt ? altText : 'not found'
+        return coverArt
+    })
+
 </script>
 
 <svelte:options runes={true} />
 
 <!-- {#if coverArtArchiveImgUrl ?? lastFmImgUrl}
     <img src={lastFmImgUrl ?? coverArtArchiveImgUrl ?? wave} alt={altText} class={imgClass} /> -->
-{#if coverArtItem['artist_name'] && coverArtItem['artist_name'] != null}
+
+<!-- {#if coverArtItem['artist_name'] && coverArtItem['artist_name'] != null}
     {#await getLastFmCoverArt(coverArtItem)}
         <img src={wave} alt="loading" class={imgClass} />
     {:then result}
@@ -53,7 +65,9 @@ Currently configured to server Last.fm images on the client side by default on a
     {/await}
 {:else}
     <img src={wave} alt="not found" class={imgClass}  />
-{/if}
+{/if} -->
+
+<img src={coverArt} alt={text} class={imgClass} />
 
 <style>
     img {
