13c230444d44e80645a3b2ce8c58db825e56893f
sug48 <mpsugarman@gmail.com>
2025-01-14 19:11:53 -0500

    fat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,10 +11,8 @@
 	import "$lib/styles/profile.css";
 	import "$lib/styles/themes.css";
 
-	import { goto, invalidate, invalidateAll } from '$app/navigation'
-	import { onMount, onDestroy } from 'svelte'
-
-	import { profileStoresObject } from '$lib/stores.ts'
+	import { onMount } from 'svelte' 
+	import { invalidate } from '$app/navigation'
 	import Header from "src/lib/components/layout/NavHeader.svelte"
 	import type { Snippet } from 'svelte'
 	import type { LayoutData } from "./$types"
@@ -22,26 +20,6 @@
 
 	let { data, children }: { data: LayoutData, children: Snippet} = $props()
 	let { session, sessionUserId, supabase } = $derived(data)
-	// export let data: LayoutData
-	// let { session, sessionUserId, user, profile, supabase } = data
-	// $: ({ session, sessionUserId, user, profile, supabase } = data)
-
-	// let { children } = $props()
-
-	// let username: string
-	// let displayName: string
-	// let avatarUrl: string
-	// let avatarArtist: string
-	// let avatarReleaseGroup: string
-	// let avatarItem = {}
-	// $: username, displayName, avatarUrl, avatarArtist, avatarReleaseGroup, avatarItem
-
-	// let username = $state() as string
-	// let displayName: string = $state() as string
-	// let avatarUrl: string = $state() as string
-	// let avatarArtist: string = $state() as string
-	// let avatarReleaseGroup: string = $state() as string
-	// let avatarItem = $state({}) as App.Lookup
 
 	let { profile } = $derived(data) as App.RowData
 
@@ -61,21 +39,7 @@
 		'release_group_name': avatar_release_group_name
 	}) as App.StringLookupObject
 
-
-	// if (typeof window !== 'undefined') {   
-	// 	username = profile?.username as string
-	// 	displayName = profile?.display_name as string
-	// 	avatarUrl = profile?.avatar_url as string
-	// 	avatarArtist = profile?.avatar_artist_name as string
-	// 	avatarReleaseGroup = profile?.avatar_release_group_name as string
-	// 	avatarItem = {
-	// 		'img_url': avatarUrl,
-	// 		'artist_name': avatarArtist,
-	// 		'release_group_name': avatarReleaseGroup
-	// 	}
-	// }
-
-	$effect(() => {
+	onMount(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
 			if (newSession?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
