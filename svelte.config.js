bf2185d175bfecc2154d0ef90f92367609d8c160
sug48 <mpsugarman@gmail.com>
2025-03-05 12:02:03 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -21,19 +21,19 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(
-		// 	{
-		// 	routes: {
-		// 		include: ['/*'],
-		// 		exclude: ['<all>']
-		// 	},
-		// 	platformProxy: {
-		// 		configPath: 'wrangler.jsonc',
-		// 		environment: undefined,
-		// 		experimentalJsonConfig: false,
-		// 		persist: false,
-		// 		platform: 'node',
-		// 	}
-		// }
+			{
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.jsonc',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false,
+				platform: 'node',
+			}
+		}
 		),
 		alias: {
 			'src/*': 'src/*',
