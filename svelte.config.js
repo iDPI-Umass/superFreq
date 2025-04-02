556b61a71a7e54046ccef6e1bf692a3dff79e03b
sug48 <mpsugarman@gmail.com>
2025-04-02 12:54:31 -0400

    XSS resistance configuration

--- svelte.config.js
+++ svelte.config.js
@@ -38,6 +38,19 @@ const config = {
 		alias: {
 			'src/*': 'src/*',
 			'./$types': './$types'
+		},
+		csp: {
+			directives: {
+				'script-src': ['self']
+			},
+			// must be specified with either the `report-uri` or `report-to` directives, or both
+			reportOnly: {
+				'script-src': ['self'],
+				'report-uri': ['/']
+			}
+		},
+		csrf: {
+			checkOrigin: true
 		}
 	}
 };
