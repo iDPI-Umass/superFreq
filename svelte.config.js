c37c32a7baccfc65b4c09dde35fb725fb609ac35
sug48 <mpsugarman@gmail.com>
2025-06-25 16:15:34 -0400

    deletions

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +7,6 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		vitePreprocess(), 
 		mdsvex({ 
 			extensions: ['.svx', '.md'],
 		})
