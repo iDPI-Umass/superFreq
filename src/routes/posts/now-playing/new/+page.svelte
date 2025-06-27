f7b8a593add9b8e9f61287a7a222ab2620cfd020
sug48 <mpsugarman@gmail.com>
2025-06-27 16:29:30 -0400

    now playing post bug

--- src/routes/posts/now-playing/new/+page.svelte
+++ src/routes/posts/now-playing/new/+page.svelte
@@ -3,9 +3,6 @@
 	import SEO from '$lib/components/layout/SEO.svelte'
     import NewNowPlayingPost from 'src/lib/components/Posts/NewNowPlayingPost.svelte'
 	import { promiseStates } from '$lib/resources/states.svelte';
-	
-	let addedItem = $state({})
-
 
     const { data } = $props()
     const { parsedUrlInfo } = $derived(data)
@@ -13,6 +10,7 @@
 	onMount(() => {
 		promiseStates.imgPromise =  null
 	})
+
 </script>
 
 <SEO title="New Now Playing post"></SEO>
